ddcf21a15415f8e03ff0b50d8f9b1692269bd0bc
josemvcerqueira <jose@ilovemochi.com>
2022-08-03 16:24:23 +0100

    feat: add interest protocol library

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
 
 const config: HardhatUserConfig = {
   solidity: {

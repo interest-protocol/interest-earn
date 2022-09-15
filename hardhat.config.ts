2f7145fedf8a10cefd0729145dff68fc260553c4
Jose Cerqueira <jose@interestprotocol.com>
2022-09-15 15:49:25 +0100

    feat: dinero vault contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import '@nomiclabs/hardhat-etherscan';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.16',
+    version: '0.8.17',
     settings: {
       optimizer: {
         enabled: true,
